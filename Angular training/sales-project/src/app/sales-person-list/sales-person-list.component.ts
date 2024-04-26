54d06bb341a32316a6ae6ceeeca8898cacf517d5
sai sumanth reddy gurrala <saisumanthreddy76@gmail.com>
2024-04-26 18:58:30 -0400

    Learned Angular
    Created a new Angular Application.

---
+++ Angular training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-person-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './sales-person-list.component.html',
+  styleUrl: './sales-person-list.component.css'
+})
+export class SalesPersonListComponent {
+
+}
