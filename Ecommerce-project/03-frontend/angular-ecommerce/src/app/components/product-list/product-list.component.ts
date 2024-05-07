23febc2809d532fc81c7cdf47ed60d20f92c3ff9
sai sumanth reddy gurrala <saisumanthreddy76@gmail.com>
2024-05-07 16:42:21 -0400

    Search feature
    1. Modified spring boot app to add a new search method.
    2. Created new component for search.
    3. Added new angular route for searching.
    4. Updated search component to send data to search route.
    5. Enhanced product list component to search for products with product services.
    6. Updated product service to call url on spring boot app.
    7. Added angular bootstrap element to show if there is no items found.

--- Ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit{
 
   products: Product [] = [];
   currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) {}
@@ -24,6 +25,27 @@ export class ProductListComponent implements OnInit{
 
   listProducts() {
 
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if(this.searchMode) {
+      this.handleSearchProducts();
+    }else{
+      this.handleListProducts();    
+    }
+  }
+
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // Now search for the products using keywords
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProducts() {
     // check if the 'id' parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
